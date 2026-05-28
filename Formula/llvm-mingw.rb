0a323061060acf1930a6377694ae8ca50ba152c2
teiosteppa <github@teiostep.com>
2026-05-28 12:34:39 -0700

    chore: update llvm-mingw to 20260519

--- Formula/llvm-mingw.rb
+++ Formula/llvm-mingw.rb
@@ -4,8 +4,8 @@
 class LlvmMingw < Formula
   desc "An LLVM/Clang/LLD based mingw-w64 toolchain"
   homepage "https://github.com/mstorsjo/llvm-mingw"
-  url "https://github.com/mstorsjo/llvm-mingw/releases/download/20260311/llvm-mingw-20260311-ucrt-macos-universal.tar.xz"
-  sha256 "42715d845b8f92941b1226d5d648c7de82dd79d67613209fea8397cf78d955a0"
+  url "https://github.com/mstorsjo/llvm-mingw/releases/download/20260519/llvm-mingw-20260519-ucrt-macos-universal.tar.xz"
+  sha256 "09a4d4c014741d92409d62c0a76541dc0d61cbfa79fc7f9220211523a4e01f2b"
   license "ISC"
   
   keg_only :provided_by_macos
@@ -16,7 +16,7 @@ class LlvmMingw < Formula
 
   test do
     resource "run-tests.sh" do
-      url "https://github.com/mstorsjo/llvm-mingw/raw/refs/tags/20251216/run-tests.sh"
+      url "https://github.com/mstorsjo/llvm-mingw/raw/refs/tags/20260519/run-tests.sh"
       sha256 "77a71ea6f2d60f0f9be780863d52791bcf60c5c3e163b7d390bfc41e4b265d69"
     end
 
